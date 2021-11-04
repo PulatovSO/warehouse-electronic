29ddb19f897ddc7f26f05376e31672c8b275510a
SO Pulatov <SirotilloPulatov@gmail.com>
2021-11-04 08:09:03 +0500

    added hero section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let wrapper = document.querySelector(".wrapper-body")
+let navbar = document.querySelector('.header__tab-wrap')
+
+function menuOpen() {
+  wrapper.classList.toggle('body-drag')
+}
+
+function menuClose() {
+  wrapper.classList.toggle('body-drag')
+
+}
+
